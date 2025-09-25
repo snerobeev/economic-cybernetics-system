9d74c26e7d1bec4998938f7bf6e85c47aadf7ad8
Sergei Nerobeev <sergeinerobeev@gmail.com>
2025-09-25 23:28:24 +0200

    added test in PlanIndicatorTest

--- src/test/java/dev/nerobeev/economic_cybernetics_system/utest/entity/PlanIndicatorTest.java
+++ src/test/java/dev/nerobeev/economic_cybernetics_system/utest/entity/PlanIndicatorTest.java
@@ -6,8 +6,11 @@ import dev.nerobeev.economic_cybernetics_system.entity.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Plan Indicator Tests")
 public class PlanIndicatorTest {
@@ -31,6 +34,22 @@ public class PlanIndicatorTest {
     @Nested
     @DisplayName("Deviation Calculations")
     class DeviationCalculations {
+        @Test
+        @DisplayName("Should calculate positive deviation correctly")
+        void shouldCalculatePositiveDeviationCorrectly() {
+
+            indicator.setActualOutput(new BigDecimal(1100));
+
+            BigDecimal deviation = indicator.getDeviation();
+            BigDecimal deviationPercent = indicator.getDeviationPercent();
+
+            assertEquals(new BigDecimal(100),deviation);
+        }
+        @Test
+        @DisplayName("Should calculate negative deviation correctly")
+        void shouldCalculateNegativeDeviationCorrectly() {
+
+        }
     }
 
 }
